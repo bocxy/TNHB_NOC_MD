61e475216c50a03f9f637acb0b924272776624c9
bocxy <sapna@bocxy.com>
2023-05-26 00:43:14 +0530

    first commit

---
+++ vendors/scripts/knob-chart-setting.js
@@ -0,0 +1,33 @@
+$(".dial1").knob();
+$({animatedVal: 0}).animate({animatedVal: 0}, {
+	duration: 3000,
+	easing: "swing",
+	step: function() {
+		$(".dial1").val(Math.ceil(this.animatedVal)).trigger("change");
+	}
+});
+$(".dial2").knob();
+$({animatedVal: 0}).animate({animatedVal: 1900}, {
+	duration: 3000,
+	easing: "swing",
+	step: function() {
+		$(".dial2").val(Math.ceil(this.animatedVal)).trigger("change");
+	}
+});
+$(".dial3").knob();
+$({animatedVal: 0}).animate({animatedVal: 400}, {
+	duration: 3000,
+	easing: "swing",
+	step: function() {
+		$(".dial3").val(Math.ceil(this.animatedVal)).trigger("change");
+	}
+});
+$(".dial4").knob();
+$({animatedVal: 0}).animate({animatedVal: 0}, {
+	duration: 3000,
+	easing: "swing",
+	step: function() {
+		$(".dial4").val(Math.ceil(this.animatedVal)).trigger("change");
+	}
+});
+$(".dial5").knob();
